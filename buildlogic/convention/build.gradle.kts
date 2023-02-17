f4cbf4e718eafedc8c9727e054b4d7d55914b7a9
jeddchoi <jed.gy.choi@gmail.com>
2023-02-18 00:37:32 +0900

    creating version catalog and migrated some gradle files to KTS

---
+++ buildlogic/convention/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    `kotlin-dsl`
+}
+group = "io.github.jeddchoi.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
