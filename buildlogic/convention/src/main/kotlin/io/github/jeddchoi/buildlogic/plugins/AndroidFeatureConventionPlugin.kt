c55f06b2994f76d0ef5c646925f1784d25a343ae
jeddchoi <jed.gy.choi@gmail.com>
2023-02-20 19:03:05 +0900

    Updated feature convention plugin dependencies

--- buildlogic/convention/src/main/kotlin/io/github/jeddchoi/buildlogic/plugins/AndroidFeatureConventionPlugin.kt
+++ buildlogic/convention/src/main/kotlin/io/github/jeddchoi/buildlogic/plugins/AndroidFeatureConventionPlugin.kt
@@ -5,6 +5,7 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.kotlin
 
 class AndroidFeatureConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -29,20 +30,20 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
 //                add("implementation", project(":core:data"))
 //                add("implementation", project(":core:common"))
 //                add("implementation", project(":core:domain"))
-//
-//                add("testImplementation", kotlin("test"))
-//                add("testImplementation", project(":core:testing"))
-//                add("androidTestImplementation", kotlin("test"))
-//                add("androidTestImplementation", project(":core:testing"))
+
+                add("testImplementation", kotlin("test"))
+                add("testImplementation", project(":core:testing"))
+                add("androidTestImplementation", kotlin("test"))
+                add("androidTestImplementation", project(":core:testing"))
 //
 //                add("implementation", libs.findLibrary("coil.kt").get())
 //                add("implementation", libs.findLibrary("coil.kt.compose").get())
 //
-//                add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
-//                add("implementation", libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
-//                add("implementation", libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
-//
-//                add("implementation", libs.findLibrary("kotlinx.coroutines.android").get())
+                add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
+
+                add("implementation", libs.findLibrary("kotlinx.coroutines.android").get())
             }
         }
     }
