85f34cf301371e62a9b894df42e8f9a556175656
jeddchoi <jed.gy.choi@gmail.com>
2023-02-20 13:40:11 +0900

    Suppress libs(bug of Android Studio)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+@file:Suppress("DSL_SCOPE_VIOLATION") // TODO: delete these suppress when updating Gradle 8.1+
 
 plugins {
-    @Suppress("DSL_SCOPE_VIOLATION") // TODO: delete these suppress when updating Gradle 8.1+
     alias(libs.plugins.android.application) apply false
-    @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.kotlin.jvm) apply false
-    @Suppress("DSL_SCOPE_VIOLATION")
     alias(libs.plugins.android.library) apply false
 }
