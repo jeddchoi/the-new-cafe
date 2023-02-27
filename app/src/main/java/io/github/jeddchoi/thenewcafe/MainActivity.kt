eff7f7c32dd969aa48f536c8115a5306da937f8f
jeddchoi <jed.gy.choi@gmail.com>
2023-02-27 18:37:01 +0900

    setDecorFitsSystemWindow = false

--- app/src/main/java/io/github/jeddchoi/thenewcafe/MainActivity.kt
+++ app/src/main/java/io/github/jeddchoi/thenewcafe/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.jeddchoi.designsystem.TheNewCafeTheme
 
@@ -21,6 +22,7 @@ import io.github.jeddchoi.designsystem.TheNewCafeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             TheNewCafeTheme {
                 // A surface container using the 'background' color from the theme
