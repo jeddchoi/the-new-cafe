1557cfa1c33db5b43bd4c8909181495b7fd98841
jeddchoi <jed.gy.choi@gmail.com>
2023-06-08 11:30:34 +0900

    Added SeatId

---
+++ firebase/functions/src/model/SeatId.ts
@@ -0,0 +1,15 @@
+type SeatId = {
+    storeId: string;
+    sectionId: string;
+    seatId: string;
+}
+
+// SeatId 객체를 문자열로 직렬화하는 함수
+function serializeSeatId(seat: SeatId): string {
+    return JSON.stringify(seat);
+}
+
+// 직렬화된 문자열을 SeatId 객체로 역직렬화하는 함수
+function deserializeSeatId(serializedSeat: string): SeatId {
+    return JSON.parse(serializedSeat) as SeatId;
+}
