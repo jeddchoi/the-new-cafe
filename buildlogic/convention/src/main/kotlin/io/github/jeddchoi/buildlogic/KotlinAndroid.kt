b5938127d74875a141277ab016201d9c3118b4b0
jeddchoi <jed.gy.choi@gmail.com>
2023-06-14 01:02:46 +0900

    dependency compat, upgrade to java 17

--- buildlogic/convention/src/main/kotlin/io/github/jeddchoi/buildlogic/KotlinAndroid.kt
+++ buildlogic/convention/src/main/kotlin/io/github/jeddchoi/buildlogic/KotlinAndroid.kt
@@ -4,6 +4,7 @@ import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionAware
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
@@ -21,14 +22,14 @@ internal fun Project.configureKotlinAndroid(
 
         tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
             kotlinOptions {
-                jvmTarget = "11"
+                jvmTarget = "17"
             }
         }
 
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
 //            isCoreLibraryDesugaringEnabled = true // TODO: investigate what desugaring means
         }
 
@@ -46,8 +47,8 @@ internal fun Project.configureKotlinAndroid(
                 "-opt-in=kotlin.Experimental",
             )
 
-            // Set JVM target to 11
-            kotlinExtension.jvmToolchain(11)
+            // Set JVM target to 17
+            kotlinExtension.jvmToolchain(17)
         }
     }
 
@@ -58,5 +59,5 @@ internal fun Project.configureKotlinAndroid(
 }
 
 fun CommonExtension<*, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
-    (this as ExtensionAware).extensions.configure("kotlinOptions", block)
+    (this as ExtensionAware).extensions.configure(block)
 }
