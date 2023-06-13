30dd37cb750e53b19efba5d5cb9c3dbe54201530
jeddchoi <jed.gy.choi@gmail.com>
2023-06-14 01:02:46 +0900

    dependency compat, upgrade to java 17

--- buildlogic/convention/build.gradle.kts
+++ buildlogic/convention/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 group = "io.github.jeddchoi.buildlogic"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
