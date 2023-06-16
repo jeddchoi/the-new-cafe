add845efcd24342f283e2b64e1e07efc4c94340b
jeddchoi <jed.gy.choi@gmail.com>
2023-06-16 14:23:58 +0900

    Refactored index.ts

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,97 +1,139 @@
 // noinspection JSUnusedGlobalSymbols
 
-import {initializeApp} from "firebase-admin/app";
-
-initializeApp();
-
-import {CallableRequest, onCall, onRequest, Request} from "firebase-functions/v2/https";
-import {onDocumentWritten} from "firebase-functions/v2/firestore";
-import {onValueWritten} from "firebase-functions/v2/database";
-import {logger} from "firebase-functions/v2";
-import {auth} from "firebase-functions";
+import "./util/firebase_initialize";
 import {Response} from "express";
-
-import {UserActionRequest} from "./model/UserActionRequest";
+import {auth} from "firebase-functions";
+import {logger} from "firebase-functions/v2";
+import {onValueWritten} from "firebase-functions/v2/database";
+import {onDocumentWritten} from "firebase-functions/v2/firestore";
+import {CallableRequest, onCall, onRequest, Request} from "firebase-functions/v2/https";
+import {afterRequestHandler} from "./handle_request/after_handle_request";
 import {requestHandler} from "./handle_request/on_handle_request";
+import {BusinessResultCode} from "./model/BusinessResultCode";
 import {
     COLLECTION_GROUP_SEAT_NAME,
     COLLECTION_GROUP_SECTION_NAME,
     COLLECTION_GROUP_STORE_NAME,
 } from "./model/SeatPosition";
 
-import {throwFunctionsHttpsError} from "./util/functions_helper";
-import {REFERENCE_USER_STATE_NAME} from "./util/RealtimeDatabaseUtil";
+import {UserActionRequest} from "./model/UserActionRequest";
+import {UserStateChangeReason} from "./model/UserStateChangeReason";
+import {authUserCreatedHandler} from "./trigger/on_auth_user_created";
 import {seatWrittenHandler} from "./trigger/on_seat_written";
 import {sectionWrittenHandler} from "./trigger/on_section_written";
 import {timerWrittenHandler} from "./trigger/on_timer_written";
-import {authUserCreatedHandler} from "./trigger/on_auth_user_created";
 import {TimeoutRequest} from "./util/CloudTasksUtil";
-import {afterRequestHandler} from "./handle_request/after_handle_request";
-import {UserStateChangeReason} from "./model/UserStateChangeReason";
+import {isBusinessResultCode} from "./util/functions_helper";
 
+import {REFERENCE_USER_STATE_NAME} from "./util/RealtimeDatabaseUtil";
 
 /**
  * Callable functions
  */
 export const onHandleRequest =
-    onCall<UserActionRequest>((
-        request: CallableRequest<UserActionRequest>,
-    ) => {
-        if (request.auth === undefined) {
-            throwFunctionsHttpsError("unauthenticated", "User is not authenticated");
-        }
-        const userId = request.auth.uid;
-        const current = new Date().getTime();
-        return requestHandler(userId, request.data.requestType, request.data.seatPosition, request.data.getEndTime(current), current)
-            .then(() => {
-                logger.info("Completed Successfully.");
-                return afterRequestHandler(userId, request.data.requestType, true, UserStateChangeReason.UserAction, request.data.seatPosition, current);
-            }).catch((e) => {
-                logger.error("Some error occurred", e);
-                return afterRequestHandler(userId, request.data.requestType, false, UserStateChangeReason.UserAction, request.data.seatPosition, current);
-            });
-    });
+    onCall<UserActionRequest, Promise<BusinessResultCode>>(
+        {
+            timeoutSeconds: 30,
+        },
+        (
+            request: CallableRequest<UserActionRequest>,
+        ) => Promise.resolve().then(() => {
+            if (!request.auth) {
+                throw BusinessResultCode.UNAUTHENTICATED;
+            }
+
+            const userId = request.auth.uid;
+            const current = new Date().getTime();
+
+            return requestHandler(userId, request.data.requestType, request.data.seatPosition, request.data.getEndTime(current), current)
+                .catch((requestError) => {
+                    logger.error("On handling request, error occurred : ", requestError);
+                    return afterRequestHandler(userId, request.data.requestType, false, UserStateChangeReason.UserAction, request.data.seatPosition, current)
+                        .catch((afterRequestError) => {
+                            logger.error("After request error also occurred", afterRequestError);
+                            throw requestError;
+                        })
+                        .then(() => {
+                            throw requestError; // even if error occurred after handling request, return request error
+                        });
+                }).then(() => {
+                    logger.info("Handling request completed successfully.");
+                    return afterRequestHandler(userId, request.data.requestType, true, UserStateChangeReason.UserAction, request.data.seatPosition, current);
+                });
+        }).then(() => {
+            return BusinessResultCode.OK;
+        }).catch((err) => {
+            if (isBusinessResultCode((err))) { // business error
+                return err;
+            }
+            throw err;
+        }));
+
 
 /**
  * HTTP functions
  */
 export const onHandleRequestTest =
-    onRequest(async (req: Request, res: Response) => {
+    onRequest((req: Request, res: Response) => Promise.resolve().then(() => {
         const userId = req.body.userId as string;
         const request = UserActionRequest.fromJSON(req.body.request);
         const current = new Date().getTime();
 
         return requestHandler(userId, request.requestType, request.seatPosition, request.getEndTime(current), current)
-            .then(() => {
-                return afterRequestHandler(userId, request.requestType, true, UserStateChangeReason.UserAction, request.seatPosition, current).then(() => {
-                    logger.info("Completed Successfully.");
-                    res.status(200).send("Completed Successfully.");
-                });
-            }).catch((e) => {
-                logger.error("Some error occurred", e);
-                return afterRequestHandler(userId, request.requestType, false, UserStateChangeReason.UserAction, request.seatPosition, current).then(() => {
-                    res.status(500).send(`Some error occurred. ${e}`);
-                });
+            .catch((requestError) => {
+                logger.error("On handling request, error occurred : ", requestError);
+                return afterRequestHandler(userId, request.requestType, false, UserStateChangeReason.UserAction, request.seatPosition, current)
+                    .catch((afterRequestError) => {
+                        logger.error("After request error also occurred", afterRequestError);
+                        throw requestError;
+                    })
+                    .then(() => {
+                        throw requestError; // even if error occurred after handling request, return request error
+                    });
+            }).then(() => {
+                logger.info("Handling request completed successfully.");
+                return afterRequestHandler(userId, request.requestType, true, UserStateChangeReason.UserAction, request.seatPosition, current);
             });
-    });
+    }).then(() => {
+        res.status(200).send(BusinessResultCode.OK);
+    }).catch((err) => {
+        if (isBusinessResultCode((err))) { // business error
+            res.status(200).send(err);
+            // return err;
+        } else {
+            res.status(500).send(err);
+        }
+    }));
 
 export const onTimeout =
-    onRequest(async (req: Request, res: Response) => {
-        const timeoutRequest = req.body as TimeoutRequest;
+    onRequest(async (req: Request, res: Response) => Promise.resolve().then(() => {
 
-        return requestHandler(timeoutRequest.userId, timeoutRequest.requestType, null, null)
-            .then(() => {
-                return afterRequestHandler(timeoutRequest.userId, timeoutRequest.requestType, true, UserStateChangeReason.Timeout, null).then(() => {
-                    logger.info("Completed Successfully.");
-                    res.status(200).send("Completed Successfully.");
-                });
-            }).catch((e) => {
-                logger.error("Some error occurred", e);
-                return afterRequestHandler(timeoutRequest.userId, timeoutRequest.requestType, false, UserStateChangeReason.Timeout, null).then(() => {
-                    res.status(500).send(`Some error occurred. ${e}`);
-                });
+        const request = req.body as TimeoutRequest;
+        const userId = request.userId;
+        return requestHandler(userId, request.requestType, null, null)
+            .catch((requestError) => {
+                logger.error("On handling request, error occurred : ", requestError);
+                return afterRequestHandler(userId, request.requestType, false, UserStateChangeReason.Timeout, null)
+                    .catch((afterRequestError) => {
+                        logger.error("After request error also occurred", afterRequestError);
+                        throw requestError;
+                    })
+                    .then(() => {
+                        throw requestError; // even if error occurred after handling request, return request error
+                    });
+            }).then(() => {
+                logger.info("Handling request completed successfully.");
+                return afterRequestHandler(userId, request.requestType, true, UserStateChangeReason.Timeout, null);
             });
-    });
+    }).then(() => {
+        res.status(200).send(BusinessResultCode.OK);
+    }).catch((err) => {
+        if (isBusinessResultCode((err))) { // business error
+            res.status(200).send(err);
+        }
+        res.status(500).send(err);
+    }));
+
 
 /**
  * Triggered functions
--- firebase/functions/src/util/functions_helper.ts
+++ firebase/functions/src/util/functions_helper.ts
@@ -1,5 +1,6 @@
 import {https, logger} from "firebase-functions/v2";
 import {FunctionsErrorCode} from "firebase-functions/lib/common/providers/https";
+import {BusinessResultCode} from "../model/BusinessResultCode";
 
 /**
  * Throw a functions.https.HttpsError
@@ -9,4 +10,9 @@ import {FunctionsErrorCode} from "firebase-functions/lib/common/providers/https"
 export function throwFunctionsHttpsError(code: FunctionsErrorCode, errorMessage: string): never {
     logger.error(errorMessage);
     throw new https.HttpsError(code, errorMessage);
+}
+
+
+export function isBusinessResultCode(error: any): error is BusinessResultCode {
+    return Object.values(BusinessResultCode).includes(error);
 }
