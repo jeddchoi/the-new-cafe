19c9c8016024243097aad2773043cc88ea877aac
jeddchoi <jed.gy.choi@gmail.com>
2023-06-24 19:51:54 +0900

    now checks auth

--- firebase/functions/src/seat-finder/https/onHandleRequest.ts
+++ firebase/functions/src/seat-finder/https/onHandleRequest.ts
@@ -1,7 +1,7 @@
 import {CallableRequest, onCall} from "firebase-functions/v2/https";
 import {getEndTime, ISeatFinderRequest} from "../_model/SeatFinderRequest";
 import SeatFinderHandler from "../SeatFinderHandler";
-import {logger} from "firebase-functions/v2";
+import {https, logger} from "firebase-functions/v2";
 
 // TODO: use parameterized configuration when fixed
 const SEAT_FINDER_FUNCTION_LOCATION = "SEAT_FINDER_FUNCTION_LOCATION";
@@ -12,13 +12,12 @@ export const onHandleRequest =
             region: "asia-northeast3",
         },
         (request: CallableRequest<ISeatFinderRequest>) => {
-            // if (!request.auth?.uid) {
-            //     throw new https.HttpsError("unauthenticated", "User must be authenticated");
-            // }
-            //
+            if (!request.auth?.uid) {
+                throw new https.HttpsError("unauthenticated", "User must be authenticated");
+            }
+
             const current = Date.now();
-            // const handler = new SeatFinderHandler(request.auth.uid);
-            const handler = new SeatFinderHandler("SAMPLE_USER_ID");
+            const handler = new SeatFinderHandler(request.auth.uid);
             logger.log("onHandleRequest =======", {request: request.data});
             return handler.handleSeatFinderRequest(
                 request.data.requestType,
