2c43812e69a66f5174bfc16eb029ddac93dcfb60
jeddchoi <jed.gy.choi@gmail.com>
2023-07-04 02:06:54 +0900

    Moved ConnectionState

---
+++ app/src/main/java/io/github/jeddchoi/thenewcafe/navigation/root/ConnectionState.kt
@@ -0,0 +1,45 @@
+package io.github.jeddchoi.thenewcafe.navigation.root
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import io.github.jeddchoi.data.util.ConnectedState
+import io.github.jeddchoi.thenewcafe.R
+
+@Composable
+fun ConnectionState(connectedState: ConnectedState) {
+    when (connectedState) {
+        ConnectedState.CONNECTED -> {}
+        ConnectedState.FOUND_CONNECTION -> {
+            val connectingMessage = stringResource(R.string.connected_again)
+            Surface(
+                color = colorResource(id = R.color.positive_background),
+            ) {
+                Text(
+                    text = connectingMessage,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            }
+        }
+        ConnectedState.LOST -> {
+            val notConnectedMessage = stringResource(R.string.not_connected)
+            Text(
+                text = notConnectedMessage,
+                color = MaterialTheme.colorScheme.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.errorContainer)
+            )
+        }
+    }
+}
--- app/src/main/java/io/github/jeddchoi/thenewcafe/navigation/root/RootScreen.kt
+++ app/src/main/java/io/github/jeddchoi/thenewcafe/navigation/root/RootScreen.kt
@@ -1,28 +1,19 @@
 package io.github.jeddchoi.thenewcafe.navigation.root
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
-import io.github.jeddchoi.data.util.ConnectedState
 import io.github.jeddchoi.data.util.NetworkMonitor
 import io.github.jeddchoi.designsystem.component.lottie.ConfettiLottie
-import io.github.jeddchoi.thenewcafe.R
 import io.github.jeddchoi.thenewcafe.navigation.main.BottomBar
 
 /**
@@ -81,33 +72,3 @@ fun RootScreen(
     }
 }
 
-@Composable
-private fun ConnectionState(connectedState: ConnectedState) {
-    when (connectedState) {
-        ConnectedState.CONNECTED -> {}
-        ConnectedState.FOUND_CONNECTION -> {
-            val connectingMessage = stringResource(R.string.connected_again)
-            Surface(
-                color = colorResource(id = R.color.positive_background),
-            ) {
-                Text(
-                    text = connectingMessage,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
-            }
-        }
-        ConnectedState.LOST -> {
-            val notConnectedMessage = stringResource(R.string.not_connected)
-            Text(
-                text = notConnectedMessage,
-                color = MaterialTheme.colorScheme.error,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(MaterialTheme.colorScheme.errorContainer)
-            )
-        }
-    }
-}
