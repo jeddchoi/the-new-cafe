55004aa633b8fdc9e44a930c8f0b33766438f436
jeddchoi <jed.gy.choi@gmail.com>
2023-07-22 23:57:37 +0900

    Added LifecycleService

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,8 @@ dependencies {
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.material3.windowSizeClass)
+    implementation(libs.androidx.lifecycle.service)
+
 
     testImplementation(kotlin("test"))
     testImplementation(project(":core:testing"))
