9f100942c4371e65647abef044595770cdf118e7
jeddchoi <jed.gy.choi@gmail.com>
2023-08-04 22:28:53 +0900

    onDisconnectedOnOccupied

--- firebase/functions/src/_firestore/model/Seat.ts
+++ firebase/functions/src/_firestore/model/Seat.ts
@@ -3,6 +3,7 @@ import {SeatStateType} from "../../seat-finder/_enum/SeatStateType";
 export interface Seat {
     name: string;
     minor: string;
+    macAddress: string;
     state: SeatStateType;
     isAvailable: boolean;
     userId: string | null;
